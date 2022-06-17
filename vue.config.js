a489e67aefd163031efb367e8e2480a4ec3ace27
ivan.hryshko <ihryshko@datrics.ai>
2022-06-17 09:18:29 +0300

    added public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // publicPath: process.env.NODE_ENV === "production" ? "/vue-test/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/branch-name-generator/" : "/",
   transpileDependencies: true,
 })
