eb0344a189ba7953ef5213fdb15e7c3583bf731f
ivan.hryshko <ihryshko@datrics.ai>
2022-06-18 12:04:56 +0300

    move result to block-result

---
+++ src/components/block-result/block-result.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="result__block">
+    <div
+      class="result__value"
+      @click="copyURL(text)"
+    >
+      {{ text }}
+    </div>
+    <ImageCopy
+      @click="copyURL(text)"
+    />
+  </div>
+</template>
+
+<script>
+import ImageCopy from '@/assets/image-copy.vue'
+
+export default {
+  name: 'BlockResult',
+  components: {
+    ImageCopy,
+  },
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+  setup() {
+    function copyURL(value) {
+      // document.execCommand('copy')
+      navigator.clipboard.writeText(value).then(() => {
+        /* clipboard successfully set */
+      }, () => {
+        /* clipboard write failed */
+      })
+    }
+
+    return {
+      copyURL,
+    }
+  },
+}
+</script>
---
+++ src/components/block-result/index.js
@@ -0,0 +1,1 @@
+export { default } from './block-result.vue'
